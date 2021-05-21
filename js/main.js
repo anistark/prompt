a20fcf745b9fd0d5f71f313163982dc280c29037
Kumar Anirudha <anirudhastark@yahoo.com>
2021-05-21 16:18:55 +0530

    updated home page and doc page [wip].

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ var getAll = function (selector, scope) {
 // setup typewriter effect in the terminal demo
 if (document.getElementsByClassName('demo').length > 0) {
   var i = 0;
-  var txt = `prompt
+  var txt = `npm install prompt
             [Entry mode; press Ctrl+D to save and quit; press Ctrl+C to quit without saving]
 
             ###todo for new year dinner party
